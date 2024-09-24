7626bf7066aa3bdedf025e7f63676dac22b1611c
mohammedabuiriban <m.g.iriban@gmail.com>
2024-09-24 16:38:17 +0200

    use puppeteer stealth plugin

--- src/Client.js
+++ src/Client.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const EventEmitter = require('events');
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin());
 const moduleRaid = require('@pedroslopez/moduleraid/moduleraid');
 
 const Util = require('./util/Util');
